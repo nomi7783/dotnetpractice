e599357f4db3598190e9550decfec5b1cef18f8a
nomi7783 <muhammadnomankhankhan@gmail.com>
2021-07-01 15:54:13 +0500

    add formvalidator

---
+++ formvalidator/formValidator.aspx
@@ -0,0 +1,65 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="formValidator.aspx.cs" Inherits="DotNetPractice.formValidator" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <script src="Scripts/jquery-3.4.1.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $(document).on("click", "#btnSubmit", function () {
+
+                var bValid = true;
+
+                var txtName = $("#txtName");
+                var Label1 = $("#Label1");
+
+                Label1.text("");
+
+                if (txtName.val() == "") {
+                    Label1.text("please enter your name");
+                    Label1.css("color", "red");
+                    bValid = false;
+                }
+
+                var txtCNIC = $("#txtCNIC");
+                var Label2 = $("#Label2");
+
+                Label2.text("");
+
+                if (txtCNIC.val() == "") {
+                    Label2.text("please enter your name");
+                    bValid = false;
+                }
+
+                return bValid;
+            });
+        });
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <asp:Label ID="lblName" runat="server" Text="Name"></asp:Label>
+            <asp:TextBox ID="txtName" runat="server" MaxLength="10"></asp:TextBox>
+            <asp:Label ID="Label1" runat="server"></asp:Label>
+
+            <br />
+            <br />
+
+            <asp:Label ID="lblCNIC" runat="server" Text="CNIC"></asp:Label>
+            <asp:TextBox ID="txtCNIC" runat="server" MaxLength="13"></asp:TextBox>
+            <asp:Label ID="Label2" runat="server"></asp:Label>
+
+            <br />
+            <br />
+
+            <asp:Button ID="btnSubmit" runat="server" Text="Add" OnClick="btnSubmit_Click1" />
+
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ formvalidator/formValidator.aspx.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DotNetPractice
+{
+    public partial class formValidator : System.Web.UI.Page
+    {
+        //=============
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        //=============
+        protected void btnSubmit_Click1(object sender, EventArgs e)
+        {
+            /*int result = 0;
+            bool bCNIC = int.TryParse(txtCNIC.Text, out result);
+
+            char[] aName = txtName.Text.ToCharArray();
+            bool bName = true;
+            foreach (char name in aName)
+            {
+                if (!char.IsLetter(name))
+                {
+                    bName = false;
+                }
+            }
+
+            if (bCNIC && bName)
+            {
+                Log(txtName.Text, txtCNIC.Text);
+                ScriptManager.RegisterClientScriptBlock(Page, Page.GetType(), "FormSubmit", "alert('Your data is added');", true);
+            }
+            else
+            {
+                ScriptManager.RegisterClientScriptBlock(Page, Page.GetType(), "FormSubmit", "alert('" + (!bName ? "name failed" : "") + (!bCNIC ? "cnic failed" : "") + "');", true);
+            }*/
+
+            double result;
+            bool bCNIC = double.TryParse(txtCNIC.Text, out result);
+            char[] aName = txtName.Text.ToCharArray();
+            bool bName = true;
+
+
+            Label1.Text = "";
+            Label2.Text = "";
+
+            if (string.IsNullOrEmpty(txtName.Text))
+            {
+                bName = false;
+                Label1.Text = "Please Enter Your Name";
+            }
+
+            if (string.IsNullOrEmpty(txtCNIC.Text))
+            {
+                bName = false;
+                Label2.Text = "Please Enter Your CNIC";
+            }
+
+            if (!bCNIC)
+            {
+                Label2.Text = "Incorrect CNIC";
+            }
+
+
+            foreach (char name in aName)
+            {
+                if (!char.IsLetter(name))
+                {
+                    bName = false;
+                    Label1.Text = "Incorrect Name";
+                }
+            }
+
+            if (bCNIC && bName)
+            {
+                ScriptManager.RegisterClientScriptBlock(Page, Page.GetType(), "FormSubmit", "alert('Your data is added');", true);
+            }
+            else
+            {
+                ScriptManager.RegisterClientScriptBlock(Page, Page.GetType(), "FormSubmit", "alert('FAILED');", true);
+            }
+
+        }
+
+        //=============
+     /*   public void Log(string name, string cnic)
+        {
+            string path = @"C:\Users\user\source\repos\ApiHitPractice\DotNetPractice\logdata\" + cnic + ".txt";
+            if (!File.Exists(path))
+            {
+                File.Create(path).Close();
+            }
+            using (FileStream fs = new FileStream(path, FileMode.Append, FileAccess.Write, FileShare.ReadWrite))
+            {
+                using (StreamWriter writer = new StreamWriter(fs))
+                {
+                    writer.WriteLine("Name = " + name + Environment.NewLine);
+                    writer.WriteLine("CNIC = " + cnic + Environment.NewLine);
+                    writer.Flush();
+                    writer.Close();
+                }
+            }
+        } */
+
+        
+    }
+}
