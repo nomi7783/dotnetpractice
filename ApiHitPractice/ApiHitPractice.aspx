4b2b5d2a291ebc4a08f985d2033dad6fc71c1891
nomi7783 <muhammadnomankhankhan@gmail.com>
2021-07-01 11:38:38 +0500

    first commit

---
+++ ApiHitPractice/ApiHitPractice.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ApiHitPractice.aspx.cs" Inherits="DotNetPractice.ApiHitPractice" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView ID="GridView1" CssClass="tableCustomizer" runat="server" AutoGenerateColumns="false">
+                 <Columns>  
+                        <asp:BoundField DataField="id" HeaderText="S.No." />
+                        <asp:BoundField DataField="name" HeaderText="Name" />
+                        <asp:BoundField DataField="username" HeaderText="User Name" /> 
+                     <asp:BoundField DataField="address.street" HeaderText="Street" /> 
+                     <asp:BoundField DataField="address.suite" HeaderText="Suite" /> 
+                     <asp:BoundField DataField="address.city" HeaderText="City" /> 
+                     <asp:BoundField DataField="address.zipcode" HeaderText="Zipcode" /> 
+                     <asp:BoundField DataField="address.geo.lat" HeaderText="lat" /> 
+                     <asp:BoundField DataField="address.geo.lng" HeaderText="lng" /> 
+                     <asp:BoundField DataField="phone" HeaderText="Phone" /> 
+                     <asp:BoundField DataField="website" HeaderText="Website" /> 
+                     <asp:BoundField DataField="company.name" HeaderText="Name" /> 
+                     <asp:BoundField DataField="company.catchPhrase" HeaderText="catchPhrase" /> 
+                     <asp:BoundField DataField="company.bs" HeaderText="BS" /> 
+                  </Columns> 
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ApiHitPractice/ApiHitPractice.aspx.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Script.Serialization;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DotNetPractice
+{
+    public partial class ApiHitPractice : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            WebClient webClient = new WebClient();
+            string str = webClient.DownloadString("https://jsonplaceholder.typicode.com/users");
+
+
+            JavaScriptSerializer javaScriptSerializer = new JavaScriptSerializer();
+            List<users> lstUsers = javaScriptSerializer.Deserialize<List<users>>(str);
+
+
+            GridView1.DataSource = lstUsers;
+            GridView1.DataBind();
+
+
+        }
+    }
+
+    public class users
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string username { get; set; }
+        public string email { get; set; }
+        public address address { get; set; }
+        public string phone { get; set; }
+        public string website { get; set; }
+        public company company { get; set; }
+    }
+
+    public class address
+    {
+        public string street { get; set; }
+        public string suite { get; set; }
+        public string city { get; set; }
+        public string zipcode { get; set; }
+
+        public geo geo { get; set; }
+
+    }
+
+    public class geo
+    {
+        public string lat { get; set; }
+
+        public string lng { get; set; }
+    }
+
+    public class company
+    {
+        public string name { get; set; }
+
+        public string catchPhrase { get; set; }
+
+        public string bs { get; set; }
+
+    }
+}
